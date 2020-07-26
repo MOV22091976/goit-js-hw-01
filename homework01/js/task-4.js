7e0c48814e3e8fc8a3c318448a69ded625b8484f
MOV22091976 <sas_moskvin@i.ua>
2020-07-26 13:32:16 +0300

    homework01

--- homework01/js/task-4.js
+++ homework01/js/task-4.js
@@ -3,14 +3,14 @@
 const credits = 23580;
 const pricePerDroid = 3000;
 const isBuyOrder = prompt('Сколько дроидов хотите купить?', '');
-const totalPrice = isBuyOrder * pricePerDroid;
 
-if (isBuyOrder) {
+if (isBuyOrder === null) {
+    alert('Отменено пользователем!');
+} else {
+    const totalPrice = isBuyOrder * pricePerDroid
     if (totalPrice <= credits) {
         alert(`Вы купили ${isBuyOrder} дроидов, на счету осталось ${credits - totalPrice} кредитов.`);
     } else {
-        alert('Недостаточно средств на счету!');
+        alert('Недостаточно средств на счету!')
     }
-} else {
-    alert('Отменено пользователем!');
 }
